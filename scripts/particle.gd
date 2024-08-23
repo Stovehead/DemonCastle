7ad481ea8bc8f446e537743ad13687d8661dd64e
Test Student <teststudent388@gmail.com>
2024-08-23 10:07:19 -0400

    made particle generic

--- scripts/particle.gd
+++ scripts/particle.gd
@@ -1,7 +1,8 @@
+class_name Particle
 extends CharacterBody2D
 
-const DESPAWN_THRESHOLD:int = 200
 @onready var gravity_component:GravityComponent = $GravityComponent
+@onready var sprite:Sprite2D = $Sprite2D
 
 func _physics_process(delta: float) -> void:
 	velocity = gravity_component.apply_gravity(velocity, delta)
--- scripts/water_particle_spawner.gd
+++ scripts/water_particle_spawner.gd
@@ -1,20 +1,24 @@
 extends Area2D
 
-@onready var water_particle:PackedScene = preload("res://scenes/water_particle.tscn")
+@onready var particle:PackedScene = preload("res://scenes/particle.tscn")
+@onready var water_particle_texture:Texture = preload("res://media/graphics/water_particle.png")
 
 func spawn_particles(pos:Vector2):
-	var particle1:CharacterBody2D = water_particle.instantiate()
+	var particle1:Particle = particle.instantiate()
 	particle1.position = pos
 	particle1.velocity = Vector2(10, -130)
-	var particle2:CharacterBody2D = water_particle.instantiate()
+	var particle2:Particle = particle.instantiate()
 	particle2.position = pos
 	particle2.velocity = Vector2(25, -30)
-	var particle3:CharacterBody2D = water_particle.instantiate()
+	var particle3:Particle = particle.instantiate()
 	particle3.position = pos
 	particle3.velocity = Vector2(-25, -30)
 	add_child(particle1)
 	add_child(particle2)
 	add_child(particle3)
+	particle1.sprite.texture = water_particle_texture
+	particle2.sprite.texture = water_particle_texture
+	particle3.sprite.texture = water_particle_texture
 
 
 func _on_body_entered(body: Node2D) -> void:
