85cd1e4b73ede0267e3707d4ffe84df2e62fd59d
Stovehead <leventistifli@gmail.com>
2024-08-25 15:51:41 -0400

    Refactored zombie spawner script

--- scripts/zombie_spawner.gd
+++ scripts/zombie_spawner.gd
@@ -14,28 +14,25 @@ var ray_right:RayCast2D
 @export var top:float = -32
 @export var bottom:float = 108
 
+func init_ray(x_position:float) -> RayCast2D:
+	var ray:RayCast2D = RayCast2D.new()
+	add_child(ray)
+	ray.position.x = x_position
+	ray.position.y = top
+	ray.target_position.x = 0
+	ray.target_position.y = bottom - top
+	ray.set_collision_mask_value(1, false)
+	ray.set_collision_mask_value(2, true)
+	return ray
+
 func init_rays() -> void:
-	ray_left = RayCast2D.new()
-	ray_right = RayCast2D.new()
-	add_child(ray_right)
-	add_child(ray_left)
-	ray_right.position.x = horizontal_range
-	ray_left.position.x = -horizontal_range
-	ray_right.position.y = top
-	ray_left.position.y = top
-	ray_right.target_position.x = 0
-	ray_left.target_position.x = 0
-	ray_right.target_position.y = bottom - top
-	ray_left.target_position.y = bottom - top
-	ray_right.set_collision_mask_value(1, false)
-	ray_right.set_collision_mask_value(2, true)
-	ray_left.set_collision_mask_value(1, false)
-	ray_left.set_collision_mask_value(2, true)
+	ray_left = init_ray(-horizontal_range)
+	ray_right = init_ray(horizontal_range)
 
 func is_valid_spawn(ray:RayCast2D) -> bool:
 	return ray.is_colliding() && ray.get_collision_point().y > top - ZOMBIE_HALF_HEIGHT && ray.get_collision_point().x > Globals.game_instance.current_stage.position.x + Globals.game_instance.current_stage.left_limit && ray.get_collision_point().x < Globals.game_instance.current_stage.position.x + Globals.game_instance.current_stage.right_limit 
 
-func spawn(spawn_position:Vector2):
+func spawn(spawn_position:Vector2) -> void:
 	var new_zombie:Zombie = zombie.instantiate()
 	add_sibling(new_zombie)
 	new_zombie.global_position = spawn_position - Vector2(0, ZOMBIE_HALF_HEIGHT)
