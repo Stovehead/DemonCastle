7aaa6c82fe5b478f4d19dd4ecbb9775a03ceb5a2
Stovehead <leventistifli@gmail.com>
2024-08-25 15:16:28 -0400

    Refactored player attack script

--- scripts/player_attack.gd
+++ scripts/player_attack.gd
@@ -3,9 +3,9 @@ extends Hurtbox
 
 @onready var hit_spark:PackedScene = preload("res://scenes/hit_spark.tscn")
 
-func _on_area_entered(area:Area2D):
+func _on_area_entered(area:Area2D) -> void:
 	SfxManager.play_sound_effect_no_overlap(SfxManager.HIT)
-	var new_spark = hit_spark.instantiate()
+	var new_spark:Node2D = hit_spark.instantiate()
 	new_spark.global_position.y = global_position.y
 	Globals.game_instance.current_stage.add_child(new_spark)
 	if(global_position.x > area.global_position.x):
