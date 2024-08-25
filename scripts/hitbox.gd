09dbf4c1af490073823a15cd9eb9ad47bb2458f8
Stovehead <leventistifli@gmail.com>
2024-08-25 15:09:49 -0400

    Refactored hitbox script

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -6,11 +6,12 @@ extends Area2D
 
 signal got_hit(attacker:Hurtbox)
 
-func _on_area_entered(area):
-	if(area is Hurtbox):
-		if(is_instance_valid(health_component)):
-			health_component._decrease_hp(area.damage)
-		got_hit.emit(area)
+func _on_area_entered(area) -> void:
+	if(area is not Hurtbox):
+		return
+	if(is_instance_valid(health_component)):
+		health_component._decrease_hp(area.damage)
+	got_hit.emit(area)
 
 func _notification(what: int) -> void:
 	if(what == NOTIFICATION_PREDELETE && is_instance_valid(health_component) && is_instance_valid(Globals.game_instance) && health_component.remaining_hp <= 0):
