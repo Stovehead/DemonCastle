ca14dc7f508462027b58b615e17e5aba15947a5d
Stovehead <leventistifli@gmail.com>
2024-08-25 14:24:36 -0400

    Refactored big heart script

--- scripts/big_heart.gd
+++ scripts/big_heart.gd
@@ -1,7 +1,9 @@
 extends Upgrade
 
+const NUM_ADDED_HEARTS:int = 5
+
 func do_upgrade(player:Player):
-	player.num_hearts += 5
+	player.num_hearts += NUM_ADDED_HEARTS
 	player.hearts_changed.emit(player.num_hearts)
 	SfxManager.play_sound_effect(SfxManager.HEART)
 	get_parent().queue_free()
