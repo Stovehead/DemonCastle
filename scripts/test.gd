7b55d1035620d696076d8f6ca080bdc769305e04
Stovehead <leventistifli@gmail.com>
2024-08-25 15:24:44 -0400

    Removed test script

--- scripts/test.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-@onready var player:Node2D = $Player
-@onready var camera:Camera2D = $Camera
-
-func _process(delta) -> void:
-	camera.global_position = floor(player.global_position)
