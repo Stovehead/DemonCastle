5914874726599414843011c60338b35209e08640
Stovehead <leventistifli@gmail.com>
2024-08-25 15:08:30 -0400

    Refactored heatlh component script

--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -9,12 +9,12 @@ var remaining_hp:int
 func _ready() -> void:
 	remaining_hp = max_hp
 
-func _decrease_hp(decrease_amount:int):
+func _decrease_hp(decrease_amount:int) -> void:
 	if(decrease_amount > 0 && remaining_hp > 0):
 		remaining_hp = max(remaining_hp - decrease_amount, 0)
 		hp_changed.emit(remaining_hp)
 
-func _increase_hp(increase_amount:int):
+func _increase_hp(increase_amount:int) -> void:
 	if(increase_amount > 0 && remaining_hp < max_hp):
 		remaining_hp = min(remaining_hp + increase_amount, max_hp)
 		hp_changed.emit(remaining_hp)
