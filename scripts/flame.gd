66f0d157faaa617453ce85b0878acf3907ff23d1
Stovehead <leventistifli@gmail.com>
2024-08-25 15:03:21 -0400

    Refactored flame script

--- scripts/flame.gd
+++ scripts/flame.gd
@@ -10,6 +10,7 @@ enum Items{
 	MONEY_BAG_400,
 	MONEY_BAG_700,
 }
+
 @onready var droppable_items = [
 	null,
 	preload("res://scenes/small_heart.tscn"),
@@ -19,12 +20,13 @@ enum Items{
 	preload("res://scenes/money_bag_400.tscn"),
 	preload("res://scenes/money_bag_700.tscn")
 ]
+
 var item_to_drop:int = 0
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if(item_to_drop != 0):
-		var item_instance = droppable_items[item_to_drop].instantiate()
-		if(item_instance is Node2D):
-			item_instance.position = position
-		add_sibling(item_instance)
+	if(item_to_drop == 0):
+		queue_free()
+	var item_instance:Node2D = droppable_items[item_to_drop].instantiate()
+	item_instance.position = position
+	add_sibling(item_instance)
 	queue_free()
