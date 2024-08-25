ae14eeefb94b0053a5d06f935d7edc160fcd52f6
Stovehead <leventistifli@gmail.com>
2024-08-25 15:46:31 -0400

    Refactored water particle spawner script

--- scripts/water_particle_spawner.gd
+++ scripts/water_particle_spawner.gd
@@ -1,30 +1,26 @@
 extends Area2D
 
+const PARTICLE_VELOCITIES:Array = [Vector2(10, -130), Vector2(25, -30), Vector2(-25, -30)]
+
 @onready var particle:PackedScene = preload("res://scenes/particle.tscn")
 @onready var water_particle_texture:Texture = preload("res://media/graphics/water_particle.png")
 
-func spawn_particles(pos:Vector2):
+func spawn_particle(pos:Vector2, velocity:Vector2):
 	var particle1:Particle = particle.instantiate()
-	particle1.position = pos
-	particle1.velocity = Vector2(10, -130)
-	var particle2:Particle = particle.instantiate()
-	particle2.position = pos
-	particle2.velocity = Vector2(25, -30)
-	var particle3:Particle = particle.instantiate()
-	particle3.position = pos
-	particle3.velocity = Vector2(-25, -30)
+	particle1.velocity = velocity
 	add_child(particle1)
-	add_child(particle2)
-	add_child(particle3)
+	particle1.global_position = pos
 	particle1.sprite.texture = water_particle_texture
-	particle2.sprite.texture = water_particle_texture
-	particle3.sprite.texture = water_particle_texture
 
+func spawn_particles(pos:Vector2):
+	for particle_velocity:Vector2 in PARTICLE_VELOCITIES:
+		spawn_particle(pos, particle_velocity)
 
 func _on_body_entered(body: Node2D) -> void:
-	if(body is CharacterBody2D):
-		spawn_particles(Vector2(to_local(body.global_position).x, 0))
-		if(body.velocity.y <= 0):
-			SfxManager.play_sound_effect(SfxManager.WATER_EXIT)
-		else:
-			SfxManager.play_sound_effect(SfxManager.WATER_ENTER)
+	if(body is not CharacterBody2D):
+		return
+	spawn_particles(Vector2(body.global_position.x, global_position.y))
+	if(body.velocity.y <= 0):
+		SfxManager.play_sound_effect(SfxManager.WATER_EXIT)
+	else:
+		SfxManager.play_sound_effect(SfxManager.WATER_ENTER)
