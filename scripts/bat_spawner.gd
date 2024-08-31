7eae97488d737927f82dc1161177ff169ea14b4d
Test Student <leventistifli@gmail.com>
2024-08-31 10:25:52 -0400

    Added bat spawner margin

--- scripts/bat_spawner.gd
+++ scripts/bat_spawner.gd
@@ -3,6 +3,7 @@ extends Node
 
 const TIME_INTERVAL:float = 6.4
 const SPAWN_HEIGHT:float = 8.0
+const HORIZONTAL_MARGIN:float = 64.0
 
 @onready var spawn_timer:Timer = $SpawnTimer
 @onready var bat_scene:PackedScene = preload("res://scenes/bat.tscn")
@@ -11,6 +12,11 @@ const SPAWN_HEIGHT:float = 8.0
 @export var max_num_bats:int = 2
 
 var num_bats:int = 0
+var screen_half_width:float = 0
+
+func _ready() -> void:
+	if(is_instance_valid(Globals.game_instance)):
+		screen_half_width = Globals.game_instance.get_viewport_rect().size.x/2.0
 
 func _on_bat_died() -> void:
 	num_bats -= 1
@@ -27,9 +33,15 @@ func _on_spawn_timer_timeout() -> void:
 	var player_direction:float = Globals.current_player.player_direction
 	var new_bat:Bat = bat_scene.instantiate()
 	new_bat.direction = -player_direction
+	if(screen_half_width != 0):
+		var player_position:Vector2 = Globals.current_player.global_position
+		if(player_position.x < screen_center_position.x - screen_half_width + HORIZONTAL_MARGIN):
+			new_bat.direction = -1
+		elif(player_position.x > screen_center_position.x + screen_half_width - HORIZONTAL_MARGIN):
+			new_bat.direction = 1
 	new_bat.died.connect(_on_bat_died)
 	num_bats += 1
 	add_sibling(new_bat)
-	new_bat.global_position = Vector2(screen_center_position.x + radius * player_direction, player_y - SPAWN_HEIGHT)
+	new_bat.global_position = Vector2(screen_center_position.x + radius * new_bat.direction * -1, player_y - SPAWN_HEIGHT)
 	new_bat.initial_position = new_bat.global_position
 	spawn_timer.start(TIME_INTERVAL)
