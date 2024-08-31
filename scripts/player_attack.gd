84ad80912e7dc0897b14ace57fa556d30fbf76d3
Test Student <leventistifli@gmail.com>
2024-08-31 10:10:35 -0400

    Fixed player attack Y position

--- scripts/player_attack.gd
+++ scripts/player_attack.gd
@@ -8,8 +8,8 @@ func _on_area_entered(area:Area2D) -> void:
 		return
 	SfxManager.play_sound_effect_no_overlap(SfxManager.HIT)
 	var new_spark:Node2D = hit_spark.instantiate()
-	new_spark.global_position.y = global_position.y
 	Globals.game_instance.current_stage.add_child(new_spark)
+	new_spark.global_position.y = global_position.y
 	if(global_position.x > area.global_position.x):
 		new_spark.global_position.x = area.global_position.x + 4
 	else:
