1b2b2f12ed2bc9bffb33a5a506b7682e766c680b
Test Student <leventistifli@gmail.com>
2024-09-08 14:32:43 -0400

    Fixed holy water bug

--- scripts/holy_water.gd
+++ scripts/holy_water.gd
@@ -9,6 +9,8 @@ const BOTTLE_VELOCITY:Vector2 = Vector2(100, -100)
 @onready var bottle:Bottle = $Bottle
 @onready var collision:CollisionShape2D = $CollisionShape2D
 
+var bottle_active:bool = true
+
 func _ready() -> void:
 	bottle.velocity.x = BOTTLE_VELOCITY.x * direction
 	bottle.velocity.y = BOTTLE_VELOCITY.y
@@ -23,8 +25,18 @@ func _on_bottle_hit_ground(hit_position:Vector2) -> void:
 	SfxManager.play_sound_effect_no_overlap(SfxManager.HOLY_WATER)
 	global_position = hit_position
 	sprite.visible = true
+	bottle_active = false
 	animation_player.play("default")
 	collision.position = Vector2.ZERO
 	collision.shape.extents = Vector2(8, 8)
 	await get_tree().create_timer(DESPAWN_TIME, false, true).timeout
 	queue_free()
+
+func _on_area_entered(area:Area2D) -> void:
+	if(area is not Hitbox):
+		return
+	SfxManager.play_sound_effect_no_overlap(SfxManager.HIT)
+	if(bottle_active):
+		spawn_spark(bottle.global_position, area)
+	else:
+		spawn_spark(global_position, area)
--- scripts/player_attack.gd
+++ scripts/player_attack.gd
@@ -3,14 +3,17 @@ extends Hurtbox
 
 @onready var hit_spark:PackedScene = preload("res://scenes/hit_spark.tscn")
 
+func spawn_spark(spawn_position:Vector2, area:Area2D):
+	var new_spark:Node2D = hit_spark.instantiate()
+	Globals.game_instance.current_stage.add_child(new_spark)
+	new_spark.global_position.y = spawn_position.y
+	if(spawn_position.x > area.global_position.x):
+		new_spark.global_position.x = area.global_position.x + 4
+	else:
+		new_spark.global_position.x = area.global_position.x - 4
+
 func _on_area_entered(area:Area2D) -> void:
 	if(area is not Hitbox):
 		return
 	SfxManager.play_sound_effect_no_overlap(SfxManager.HIT)
-	var new_spark:Node2D = hit_spark.instantiate()
-	Globals.game_instance.current_stage.add_child(new_spark)
-	new_spark.global_position.y = global_position.y
-	if(global_position.x > area.global_position.x):
-		new_spark.global_position.x = area.global_position.x + 4
-	else:
-		new_spark.global_position.x = area.global_position.x - 4
+	spawn_spark(global_position, area)
